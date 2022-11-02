bd56499981987d092b5eb0d78aab04898967a12d
bandrzew <bandrzew@gmail.com>
2022-11-02 13:35:08 +0100

    Upgrade resteasy to 6.2.1

--- src/main/java/com/random/role/lol/ddragon/resource/DdragonResource.java
+++ src/main/java/com/random/role/lol/ddragon/resource/DdragonResource.java
@@ -1,12 +1,13 @@
 package com.random.role.lol.ddragon.resource;
 
 import com.random.role.lol.ddragon.dto.DdragonResponse;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
 import java.util.List;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 @Produces(MediaType.APPLICATION_JSON)
 public interface DdragonResource {
